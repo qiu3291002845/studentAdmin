a60a0d53e9b21185aedea429067bc5944a5288c4
MuddyRain <qiu3291002845@gmail.com>
2020-09-18 08:43:57 +0800

    初始化界面

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Main></Main>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Main: () => import("./views/main/Main"),
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/main/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <transition name="left" mode="out-in">
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+<style lang="scss">
+.left-enter {
+  transform: translateX(100%);
+}
+
+.left-enter-active {
+  transition: 0.35s;
+}
+
+.left-leave-to {
+  transform: translateX(-100%);
+}
+
+.left-leave-active {
+  transition: 0.35s;
+}
+body {
+  overflow-x: hidden;
+}
+</style>
